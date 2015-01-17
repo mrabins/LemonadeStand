7d3713c97d40387426e583d5259831e3b2a5c884
mrabins <markrabins@gmail.com>
2015-01-16 21:48:36 -0800

    Update assets

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,14 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by Mark Rabins on 1/13/15.
+//  Copyright (c) 2015 self.swift. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+    let lemon = 2
+    let iceCube = 1
+}
---
+++ LemonadeStand/Supplies.swift
@@ -0,0 +1,25 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by Mark Rabins on 1/13/15.
+//  Copyright (c) 2015 self.swift. All rights reserved.
+//
+
+import Foundation
+
+struct Supplies {
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+    init (aMoney: Int, aLemons: Int, aIceCubes: Int) {
+        money = aMoney
+        lemons = aLemons
+        iceCubes = aIceCubes
+        
+    }
+    
+    
+    
+}
